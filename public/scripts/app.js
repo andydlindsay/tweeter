30e564b64a9c9718b454df30d6b9a29ec2ac72e9
Andy Lindsay <andydlindsay@gmail.com>
2018-11-06 20:27:12 +0000

    added compose button to hide and view the new tweet form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,64 +5,75 @@
  */
 
 function createTweetElement(tweetData) {
-    const $tweetHeader = $('<header>', {});
-    const $avatar = $('<img />', {
-        src: tweetData.user.avatars.regular,
-        alt: 'User profile image'
-    });
-    const $username = $('<span>', { class: 'username' }).append(`<strong>${tweetData.user.name}</strong>`);
-    const $handle = $('<span>', { class: 'handle' }).text(tweetData.user.handle);
-    $tweetHeader.append($avatar).append($username).append($handle);
+  const $tweetHeader = $('<header>', {});
+  const $avatar = $('<img />', {
+    src: tweetData.user.avatars.regular,
+    alt: 'User profile image'
+  });
+  const $username = $('<span>', { class: 'username' }).append(`<strong>${tweetData.user.name}</strong>`);
+  const $handle = $('<span>', { class: 'handle' }).text(tweetData.user.handle);
+  $tweetHeader.append($avatar).append($username).append($handle);
 
-    const $tweetBody = $('<p>', { class: 'tweet-body' }).text(tweetData.content.text);
+  const $tweetBody = $('<p>', { class: 'tweet-body' }).text(tweetData.content.text);
 
-    const $tweetFooter = $('<footer>', {});
-    const numDaysAgo = Math.floor((new Date() - new Date(tweetData.created_at)) / 1000 / 60 / 60 / 24);
-    const $tweetDate = $('<span>', {}).text(`${numDaysAgo} days ago`);
-    const $buttonGroup = $('<span>', { class: 'button-group hidden' }).append('<i class="fas fa-flag fa-lg"></i><i class="fas fa-retweet fa-lg"></i><i class="fas fa-heart fa-lg"></i>');
-    $tweetFooter.append($tweetDate).append($buttonGroup);
+  const $tweetFooter = $('<footer>', {});
+  const numDaysAgo = Math.floor((new Date() - new Date(tweetData.created_at)) / 1000 / 60 / 60 / 24);
+  const $tweetDate = $('<span>', {}).text(`${numDaysAgo} days ago`);
+  const $buttonGroup = $('<span>', { class: 'button-group hidden' }).append('<i class="fas fa-flag fa-lg"></i><i class="fas fa-retweet fa-lg"></i><i class="fas fa-heart fa-lg"></i>');
+  $tweetFooter.append($tweetDate).append($buttonGroup);
 
-    const $tweetElement = $('<article>', { class: 'tweet' });
-    $tweetElement.append($tweetHeader).append($tweetBody).append($tweetFooter);
+  const $tweetElement = $('<article>', { class: 'tweet' });
+  $tweetElement.append($tweetHeader).append($tweetBody).append($tweetFooter);
 
-    return $tweetElement;
+  return $tweetElement;
 }
 
 $(document).ready(() => {
 
-    function renderTweets(tweets) {
-        const $tweetsContainer = $('#tweets-container');
-        $tweetsContainer.empty();
-        tweets.forEach((tweet) => {
-            $tweetsContainer.append(createTweetElement(tweet));
-        });
-    }
-
-    function loadTweets() {
-      $.get('/tweets/', (data) => {
-        renderTweets(data.reverse());
-      });
-    }
-
-    $('section.new-tweet form').submit((event) => {
-      event.preventDefault();
-      const charsUsed = $('section.new-tweet textarea')[0].textLength;
-      if (charsUsed > 140) {
-        alert('Tweet content exceeds maximum of 140 characters.');
-        return;
-      } else if (charsUsed === 0) {
-        alert('Tweet content is not present.');
-        return;
-      }
-
-      $.post('/tweets/', $('section.new-tweet form').serialize(), (err, data) => {
-        console.log('tweet sent to server');
-        loadTweets();
-        $('section.new-tweet textarea')[0].value = '';
-        $('section.new-tweet form .counter')[0].innerHTML = 140;
-      });
+  function renderTweets(tweets) {
+    const $tweetsContainer = $('#tweets-container');
+    $tweetsContainer.empty();
+    tweets.forEach((tweet) => {
+      $tweetsContainer.append(createTweetElement(tweet));
     });
+  }
 
-    loadTweets();
+  function loadTweets() {
+    $.get('/tweets/', (data) => {
+      renderTweets(data.reverse());
+    });
+  }
+
+  const $newTweetSection = $('section.new-tweet');
+  const $newTweetForm = $newTweetSection.find('form');
+  const $newTweetFormTextArea = $newTweetForm.find('textarea');
+  const $newTweetFormCounter = $newTweetForm.find('.counter');
+
+  $newTweetForm.submit((event) => {
+    event.preventDefault();
+    const charsUsed = $newTweetFormTextArea.val.textLength;
+    if (charsUsed > 140) {
+      alert('Tweet content exceeds maximum of 140 characters.');
+      return;
+    } else if (charsUsed === 0) {
+      alert('Tweet content is not present.');
+      return;
+    }
+
+    $.post('/tweets/', $newTweetForm.serialize(), (err, data) => {
+      console.log('tweet sent to server');
+      loadTweets();
+      $newTweetFormTextArea.val('');
+      $newTweetFormCounter.text('140');
+    });
+  });
+
+  loadTweets();
+
+  $('#nav-bar .button').click((event) => {
+    $newTweetSection.slideToggle(420, () => {
+      $newTweetFormTextArea.focus();
+    });
+  });
 
 });
