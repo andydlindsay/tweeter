80352a389a20940cb1bb01119c0464d7c3bc0430
Andy Lindsay <andydlindsay@gmail.com>
2018-11-06 05:53:26 +0000

    completed layout and styling for tweet elements

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,8 @@
 $(document).ready(() => {
 
     $('form textarea').on('keyup', function (event) {
-        console.log(event.currentTarget.textLength);
-        console.log(this);
         const charactersRemaining = 140 - $(this)[0].textLength;
-        $counter = $(this).parent('form').children('.counter');
+        const $counter = $(this).parent('form').children('.counter');
         $counter[0].innerHTML = charactersRemaining;
         if (charactersRemaining < 0) {
             $counter.addClass('invalid');
